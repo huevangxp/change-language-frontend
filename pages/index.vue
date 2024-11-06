be5256775df6bbc1ee8f3bb2684eb8f094c7e804
huevangxp <huevang933@gmail.com>
2024-11-06 17:28:11 +0700

    chagne language

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <div>{{ t("cryptoWallet") }}</div>
+    <v-btn @click="$store.dispatch('getTranslationData', 'th-TH')">Thai</v-btn>
+    <v-btn @click="$store.dispatch('getTranslationData', 'en-US')">Eng</v-btn>
     <!-- Product List -->
-    <div v-if="products.length">
+    <!-- <div v-if="products.length">
       <div v-for="product in products" :key="product.id" class="product-card">
         <h2>{{ currentLanguage === 'la' ? product.name_la : product.name_en }}</h2>
         <p>{{ currentLanguage === 'la' ? product.description_la : product.description_en }}</p>
         <p>{{ product.price }} {{ currentLanguage === 'la' ? 'ກີບ' : 'KIP' }}</p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -17,26 +20,27 @@ export default {
     // Get current language from Vuex store
     currentLanguage() {
       return this.$store.getters.getCurrentLanguage;
-    }
+    },
   },
   data() {
     return {
-      products: [] // Product data will be fetched from the API
+      products: [], // Product data will be fetched from the API
     };
   },
   mounted() {
     this.fetchData();
+    this.$store.dispatch("getTranslationData");
   },
   methods: {
     async fetchData() {
       try {
-        const response = await this.$axios.$get('/product');
+        const response = await this.$axios.$get("/product");
         this.products = response;
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
